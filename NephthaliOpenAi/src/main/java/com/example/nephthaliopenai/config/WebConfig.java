b9fc13bc165277b7f8f9da2c3a0fc33ed3b08056
Nephthali Tutondele <102525159+Thalituto@users.noreply.github.com>
2024-02-03 00:31:13 +0100

    Update WebConfig.java

--- NephthaliOpenAi/src/main/java/com/example/nephthaliopenai/config/WebConfig.java
+++ NephthaliOpenAi/src/main/java/com/example/nephthaliopenai/config/WebConfig.java
@@ -1,2 +1,25 @@
-package com.example.nephthaliopenai.config;public class WebConfig {
+package com.example.nephthaliopenai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
+
+
